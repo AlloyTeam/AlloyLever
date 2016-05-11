a28429f283d11ebb012f20b585abbaa749fa8285
kmdjs <kmdjs@qq.com>
2016-05-11 08:58:28 +0800

    alloylever version 0.0.1

--- dist/alloy_lever.js
+++ dist/alloy_lever.js
@@ -3163,15 +3163,26 @@ App.loadFile("component/alloy_lever/index.html", function (tpl) {
     window.AlloyLever= AlloyLever;
 });
 })();
-new AlloyLever({
-    hide:true,
-    reportTo:'xxx',
-    height:'55%',
-    index:1,
-    logs:[],
-    tx:0,
-    ty:0,
-    timeline:[],
-    xhrs:[],
-    resources:{cookie:'',storage:''}
-},'body');
+(function(){
+    try {
+        if(!document.body){
+            alert('please put the alloylever script reference between <body> and </body>');
+            return;
+        }
+        new AlloyLever({
+            hide: true,
+            reportTo: 'xxx',
+            height: '55%',
+            index: 1,
+            logs: [],
+            tx: 0,
+            ty: 0,
+            timeline: [],
+            xhrs: [],
+            resources: {cookie: '', storage: ''}
+        }, 'body');
+    }catch(e) {
+        alert(e);
+    };
+
+})()
--- fix/init_alloylever.js
+++ fix/init_alloylever.js
@@ -1,12 +1,23 @@
-new AlloyLever({
-    hide:true,
-    reportTo:'xxx',
-    height:'55%',
-    index:1,
-    logs:[],
-    tx:0,
-    ty:0,
-    timeline:[],
-    xhrs:[],
-    resources:{cookie:'',storage:''}
-},'body');
+(function(){
+    try {
+        if(!document.body){
+            alert('please put the alloylever script reference between <body> and </body>');
+            return;
+        }
+        new AlloyLever({
+            hide: true,
+            reportTo: 'xxx',
+            height: '55%',
+            index: 1,
+            logs: [],
+            tx: 0,
+            ty: 0,
+            timeline: [],
+            xhrs: [],
+            resources: {cookie: '', storage: ''}
+        }, 'body');
+    }catch(e) {
+        alert(e);
+    };
+
+})()
