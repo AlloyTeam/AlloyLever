25943cf6b31b9547d47e3229bcf9e8f58fb5b9d4
kmdjs <kmdjs@qq.com>
2016-05-13 08:57:12 +0800

    0.0.7

--- alloy_lever.js
+++ alloy_lever.js
@@ -2612,6 +2612,113 @@ App.loadFile = function (tpl, css, callback) {
     }
 }
 
+window.timing = window.timing || {
+        /**
+         * Outputs extended measurements using Navigation Timing API
+         * @param  Object opts Options (simple (bool) - opts out of full data view)
+         * @return Object      measurements
+         */
+        getTimes: function (opts) {
+            var performance = window.performance || window.webkitPerformance || window.msPerformance || window.mozPerformance;
+
+            if (performance === undefined) {
+                return false;
+            }
+
+            var timing = performance.timing;
+            var api = {};
+            opts = opts || {};
+
+            if (timing) {
+                if (opts && !opts.simple) {
+                    for (var k in timing) {
+                        if (timing.hasOwnProperty(k)) {
+                            api[k] = timing[k];
+                        }
+                    }
+                }
+
+
+                // Time to first paint
+                if (api.firstPaint === undefined) {
+                    // All times are relative times to the start time within the
+                    // same objects
+                    var firstPaint = 0;
+
+                    // Chrome
+                    if (window.chrome && window.chrome.loadTimes) {
+                        // Convert to ms
+                        firstPaint = window.chrome.loadTimes().firstPaintTime * 1000;
+                        api.firstPaintTime = firstPaint - (window.chrome.loadTimes().startLoadTime * 1000);
+                    }
+                    // IE
+                    else if (typeof window.performance.timing.msFirstPaint === 'number') {
+                        firstPaint = window.performance.timing.msFirstPaint;
+                        api.firstPaintTime = firstPaint - window.performance.timing.navigationStart;
+                    }
+                    // Firefox
+                    // This will use the first times after MozAfterPaint fires
+                    //else if (window.performance.timing.navigationStart && typeof InstallTrigger !== 'undefined') {
+                    //    api.firstPaint = window.performance.timing.navigationStart;
+                    //    api.firstPaintTime = mozFirstPaintTime - window.performance.timing.navigationStart;
+                    //}
+                    if (opts && !opts.simple) {
+                        api.firstPaint = firstPaint;
+                    }
+                }
+
+                // Total time from start to load
+                api.loadTime = timing.loadEventEnd - timing.fetchStart;
+                // Time spent constructing the DOM tree
+                api.domReadyTime = timing.domComplete - timing.domInteractive;
+                // Time consumed preparing the new page
+                api.readyStart = timing.fetchStart - timing.navigationStart;
+                // Time spent during redirection
+                api.redirectTime = timing.redirectEnd - timing.redirectStart;
+                // AppCache
+                api.appcacheTime = timing.domainLookupStart - timing.fetchStart;
+                // Time spent unloading documents
+                api.unloadEventTime = timing.unloadEventEnd - timing.unloadEventStart;
+                // DNS query time
+                api.lookupDomainTime = timing.domainLookupEnd - timing.domainLookupStart;
+                // TCP connection time
+                api.connectTime = timing.connectEnd - timing.connectStart;
+                // Time spent during the request
+                api.requestTime = timing.responseEnd - timing.requestStart;
+                // Request to completion of the DOM loading
+                api.initDomTreeTime = timing.domInteractive - timing.responseEnd;
+                // Load event time
+                api.loadEventTime = timing.loadEventEnd - timing.loadEventStart;
+
+                if (Math.abs(api.initDomTreeTime) > 1304136942)api.initDomTreeTime = 0;
+                if (Math.abs(api.loadTime) > 1304136942)api.loadTime = 0;
+            }
+
+            return api;
+        },
+        /**
+         * Uses console.table() to print a complete table of timing information
+         * @param  Object opts Options (simple (bool) - opts out of full data view)
+         */
+        printTable: function (opts) {
+            var table = {};
+            var data = this.getTimes(opts) || {};
+            Object.keys(data).sort().forEach(function (k) {
+                table[k] = {
+                    k: k,
+                    ms: data[k],
+                    s: +((data[k] / 1000).toFixed(2))
+                };
+            });
+            return data;
+        },
+        /**
+         * Uses console.table() to print a summary table of timing information
+         */
+        printSimpleTable: function () {
+            return this.printTable({simple: true});
+        }
+    };
 App.componentRes['component/alloy_lever/index.html'] =
 '<style scoped type="text/css">\
     * {\
@@ -2773,6 +2880,10 @@ App.componentRes['component/alloy_lever/index.html'] =
         color:lightseagreen;\
         font-weight: bold;\
     }\
+\
+    .at-sub-dt{\
+        text-indent :20px;\
+    }\
 </style>\
 \
 <div class="at-entry" nc-id="atEntry" ontouchstart="touchStart(event)" style="transform: translate3d({{tx}}px,{{ty}}px,0px)">AlloyLever</div>\
@@ -2792,9 +2903,15 @@ App.componentRes['component/alloy_lever/index.html'] =
         <div class="at-logs {{content2}}">\
             {{#xhrs}}\
                 <div class="at-item at-item-log">\
-                    <div class="at-url"><span>[Request Url]</span><br/>{{rqsUrl}}</div>\
-                    <div class="at-url"><span>[Response Url]</span><br/>{{rspUrl}}</div>\
-                    <div class="at-url">{{url}}</div>\
+                    <div class="at-url"><div></div><span>[Request Url]</span></div>\
+                    <div class="at-sub-dt"><span>{{method}}</span>: {{rqsUrl}}</div>\
+                </div>\
+                <div class="at-item at-item-log">\
+                    <div  class="at-url"><span>[Response Url]</span></div>\
+                   <div class="at-sub-dt">{{rspUrl}}</div>\
+                </div>\
+                <div class="at-item at-item-log">\
+                    <div  class="at-url"><span>[Response Data]</span></div>\
                     <pre class="at-json"><code>{{json}}</code></pre>\
                 </div>\
             {{/xhrs}}\
@@ -2820,114 +2937,6 @@ App.componentRes['component/alloy_lever/index.html'] =
     </div>\
 </div>';
 
-window.timing = window.timing || {
-        /**
-         * Outputs extended measurements using Navigation Timing API
-         * @param  Object opts Options (simple (bool) - opts out of full data view)
-         * @return Object      measurements
-         */
-        getTimes: function (opts) {
-            var performance = window.performance || window.webkitPerformance || window.msPerformance || window.mozPerformance;
-
-            if (performance === undefined) {
-                return false;
-            }
-
-            var timing = performance.timing;
-            var api = {};
-            opts = opts || {};
-
-            if (timing) {
-                if (opts && !opts.simple) {
-                    for (var k in timing) {
-                        if (timing.hasOwnProperty(k)) {
-                            api[k] = timing[k];
-                        }
-                    }
-                }
-
-
-                // Time to first paint
-                if (api.firstPaint === undefined) {
-                    // All times are relative times to the start time within the
-                    // same objects
-                    var firstPaint = 0;
-
-                    // Chrome
-                    if (window.chrome && window.chrome.loadTimes) {
-                        // Convert to ms
-                        firstPaint = window.chrome.loadTimes().firstPaintTime * 1000;
-                        api.firstPaintTime = firstPaint - (window.chrome.loadTimes().startLoadTime * 1000);
-                    }
-                    // IE
-                    else if (typeof window.performance.timing.msFirstPaint === 'number') {
-                        firstPaint = window.performance.timing.msFirstPaint;
-                        api.firstPaintTime = firstPaint - window.performance.timing.navigationStart;
-                    }
-                    // Firefox
-                    // This will use the first times after MozAfterPaint fires
-                    //else if (window.performance.timing.navigationStart && typeof InstallTrigger !== 'undefined') {
-                    //    api.firstPaint = window.performance.timing.navigationStart;
-                    //    api.firstPaintTime = mozFirstPaintTime - window.performance.timing.navigationStart;
-                    //}
-                    if (opts && !opts.simple) {
-                        api.firstPaint = firstPaint;
-                    }
-                }
-
-                // Total time from start to load
-                api.loadTime = timing.loadEventEnd - timing.fetchStart;
-                // Time spent constructing the DOM tree
-                api.domReadyTime = timing.domComplete - timing.domInteractive;
-                // Time consumed preparing the new page
-                api.readyStart = timing.fetchStart - timing.navigationStart;
-                // Time spent during redirection
-                api.redirectTime = timing.redirectEnd - timing.redirectStart;
-                // AppCache
-                api.appcacheTime = timing.domainLookupStart - timing.fetchStart;
-                // Time spent unloading documents
-                api.unloadEventTime = timing.unloadEventEnd - timing.unloadEventStart;
-                // DNS query time
-                api.lookupDomainTime = timing.domainLookupEnd - timing.domainLookupStart;
-                // TCP connection time
-                api.connectTime = timing.connectEnd - timing.connectStart;
-                // Time spent during the request
-                api.requestTime = timing.responseEnd - timing.requestStart;
-                // Request to completion of the DOM loading
-                api.initDomTreeTime = timing.domInteractive - timing.responseEnd;
-                // Load event time
-                api.loadEventTime = timing.loadEventEnd - timing.loadEventStart;
-
-                if (Math.abs(api.initDomTreeTime) > 1304136942)api.initDomTreeTime = 0;
-                if (Math.abs(api.loadTime) > 1304136942)api.loadTime = 0;
-            }
-
-            return api;
-        },
-        /**
-         * Uses console.table() to print a complete table of timing information
-         * @param  Object opts Options (simple (bool) - opts out of full data view)
-         */
-        printTable: function (opts) {
-            var table = {};
-            var data = this.getTimes(opts) || {};
-            Object.keys(data).sort().forEach(function (k) {
-                table[k] = {
-                    k: k,
-                    ms: data[k],
-                    s: +((data[k] / 1000).toFixed(2))
-                };
-            });
-            return data;
-        },
-        /**
-         * Uses console.table() to print a summary table of timing information
-         */
-        printSimpleTable: function () {
-            return this.printTable({simple: true});
-        }
-    };
-
 App.loadFile("component/alloy_lever/index.html", function (tpl) {
     var AlloyLever = Nuclear.create({
         install: function () {
@@ -2943,6 +2952,7 @@ App.loadFile("component/alloy_lever/index.html", function (tpl) {
         initXHR:function(){
             (function(open){
                 window.XMLHttpRequest.prototype.open=function(){
+                    this.alloyLeverMethod=arguments[0];
                     this.alloyLeverUrl=arguments[1];
                     open.apply(this,arguments);
                 }
@@ -2969,7 +2979,7 @@ App.loadFile("component/alloy_lever/index.html", function (tpl) {
                 };
                 if (isAvailable) {
                     if ((xhr.status >= 200 && xhr.status < 300) || xhr.status == 304) {
-                        self.option.xhrs.push({rqsUrl:xhr.alloyLeverUrl,rspUrl:xhr.responseURL, json:JSON.stringify(JSON.parse( xhr.responseText), null, "\t")})
+                        self.option.xhrs.push({method:xhr.alloyLeverMethod,rqsUrl:xhr.alloyLeverUrl,rspUrl:xhr.responseURL, json:JSON.stringify(JSON.parse( xhr.responseText), null, "\t")})
                     }else if(xhr.status>=400) {
                         console.error(xhr.responseURL +' '+xhr.status+' ('+xhr.statusText+')')
                     }
