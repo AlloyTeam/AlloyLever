fee3fd287b50768cb680b9222bf39c8534cb17f1
kmdjs <kmdjs@qq.com>
2016-05-16 22:25:53 +0800

    remove unused code

--- dist/alloy_lever.js
+++ dist/alloy_lever.js
@@ -2645,12 +2645,6 @@ var App = {};
 
 App.componentRes = {};
 
-App.getFileExt = function(filename) {
-    var index1 = filename.lastIndexOf(".")
-    var index2 = filename.length;
-    return filename.substring(index1, index2).toLowerCase();
-}
-
 App.loadFile = function (path) {
     return App.componentRes[path];
 }
